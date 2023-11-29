5fe41bc0cad0fd1e5ca2b647eb5373ca1015e546
moises.neto <moises.neto@animaeducacao.com.br>
2023-11-28 21:17:02 -0300

    first commit

---
+++ Models/Smartphone.cs
@@ -0,0 +1,31 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+
+
+        protected Smartphone(string numero, string modelo, string iMEI, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = iMEI;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia Bolachao = new Nokia("15456", "bol", "4566", 5462);
+Bolachao.InstalarAplicativo("Calculadora");
+Bolachao.Ligar();
+Bolachao.ReceberLigacao();
+
+Iphone phone13 = new Iphone("15456", "bol", "4566", 5462);
+phone13.InstalarAplicativo("Calculadora");
+phone13.Ligar();
+phone13.ReceberLigacao();
+
